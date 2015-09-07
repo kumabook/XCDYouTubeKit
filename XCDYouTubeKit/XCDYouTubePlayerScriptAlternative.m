460c1940a22e5c13512445e68053358c2e11107e
Hiroki Kumamoto <kumabook@live.jp>
2015-09-07 20:20:27 +0900

    Set window.navigator to global object in player script

--- XCDYouTubeKit/XCDYouTubePlayerScriptAlternative.m
+++ XCDYouTubeKit/XCDYouTubePlayerScriptAlternative.m
@@ -9,6 +9,7 @@
 #import "XCDYouTubePlayerScript.h"
 #import <JavaScriptCore/JavaScriptCore.h>
 #import <Availability.h>
+#import "XCDYouTubeLogger.h"
 #if (defined(__IPHONE_OS_VERSION_MIN_REQUIRED) && __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_7_0) || \
      (defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_9)
 
@@ -32,20 +33,33 @@
 		return nil;
 
 	NSString *script = [string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	XCDYouTubeLogTrace(@"%@", script);
 	static NSString *jsPrologue = @"(function()";
 	static NSString *jsEpilogue = @")();";
-	if ([script hasPrefix:jsPrologue] && [script hasSuffix:jsEpilogue])
+	if ([script hasPrefix:jsPrologue] && [script hasSuffix:jsEpilogue]) {
 		script = [script substringWithRange:NSMakeRange(jsPrologue.length, script.length - (jsPrologue.length + jsEpilogue.length))];
+	} else {
+		XCDYouTubeLogWarning(@"Unexpected player script (not an anonymous function)");
+	}
+
 	JSContext* __context = [[JSContext alloc] init];
 	_context = __context;
 
-	for (NSString *propertyName in @[ @"window", @"document", @"navigator" ])
+	for (NSString *propertyPath in @[ @"window.navigator", @"document", @"navigator" ])
 	{
-		NSObject *globalObject = [[NSObject alloc] init];
-		[_context setObject:globalObject forKeyedSubscript:propertyName];
+		JSValue *object = [_context globalObject];
+		for (NSString *propertyName in [propertyPath componentsSeparatedByString:@"."])
+		{
+			JSValue *defaultObject = [JSValue valueWithNewObjectInContext:_context];
+			[object setValue:defaultObject forProperty:propertyName];
+			object = defaultObject;
+		}
 	}
 
 	[_context evaluateScript:script];
+	if ([_context exception]) {
+		XCDYouTubeLogWarning(@"JavaScript exception: %@", [_context exception]);
+	}
 
 	NSRegularExpression *signatureRegularExpression = [NSRegularExpression regularExpressionWithPattern:@"[\"']signature[\"']\\s*,\\s*([^\\(]+)" options:NSRegularExpressionCaseInsensitive error:NULL];
 	NSTextCheckingResult *result = [signatureRegularExpression firstMatchInString:script options:(NSMatchingOptions)0 range:NSMakeRange(0, script.length)];
@@ -59,6 +73,11 @@
 			_signatureFunction = signatureFunction;
 		}
 	}
+
+	if (!_signatureFunction) {
+		XCDYouTubeLogWarning(@"No signature function in player script");
+	}
+
 	return self;
 }
 
